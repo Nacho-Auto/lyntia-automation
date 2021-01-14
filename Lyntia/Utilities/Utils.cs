3f1e9ee91f6926c3e6064cefcd09520c18cf28eb
Diego Escudero Martin <diego-jesus.escudero@gfi.es.world>
2021-01-14 13:01:43 +0100

    Update Utils.cs

--- Lyntia/Utilities/Utils.cs
+++ Lyntia/Utilities/Utils.cs
@@ -97,7 +97,7 @@ namespace Lyntia.Utilities
         }
         public static string RandomString(int length)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.*?¿%$/()ºªÇ-Ñ";
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
