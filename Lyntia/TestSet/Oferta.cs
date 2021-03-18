9957051faa2ff2952e2aac66c1f48b14d770bc56
Nacho-Auto <76059538+Nacho-Auto@users.noreply.github.com>
2021-03-18 11:31:22 +0100

    prueba

--- Lyntia/TestSet/Oferta.cs
+++ Lyntia/TestSet/Oferta.cs
@@ -57,7 +57,7 @@ namespace Lyntia.TestSet
             commonActions.AccesoGestionCliente();
             commonCondition.AccedeGestionCliente();
 
-            // Paso 1 - Hacer click en Ofertas 
+            // Paso 1 - Hacer click en Ofertas prueba
             ofertaActions.AccesoOfertasLyntia("Mis Ofertas lyntia");
 
             TestContext.WriteLine(Utils.dataRep.getDataFromRepository("CRM_COF0001", "MESSAGE"));
