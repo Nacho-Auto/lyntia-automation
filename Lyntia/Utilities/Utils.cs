92e088b0c499aca5bc2613b9eec702c84b327101
snavaror <sergio.navarro-ortiz@inetum.world>
2021-09-09 09:38:19 +0200

    headless

--- Lyntia/Utilities/Utils.cs
+++ Lyntia/Utilities/Utils.cs
@@ -67,7 +67,7 @@ namespace Lyntia.Utilities
                 chromeOptions.AddArguments("headless");
                 chromeOptions.AddArguments("window-size=1920x1080");
 
-                driver = new ChromeDriver();//chromeOptions               
+                driver = new ChromeDriver(chromeOptions);//chromeOptions               
 
                 objRep = ObjectRepositoryUtils.Instance;
                 objRep.TestDataReader("ObjectRepository.csv");
